b603fa4f03f3c8d2a57086f503f31a88f4ec98e2
Andrey Torres Calderón <atorresf20@gmail.com>
2020-05-28 15:44:18 -0600

    R3, R4, R5, R6 completados

--- Asignacion 3a/asignacion3a.py
+++ Asignacion 3a/asignacion3a.py
@@ -1,3 +1,10 @@
+from unittest import TestCase
+#Import de la libreria datetime para conseguir la fecha del sistema
+from datetime import date
+
+dias31 = [1, 3, 5, 7, 8, 10, 12] # Enero, Marzo, Mayo, Julio, Agosto, Octubre y Diciembre meses con 31 días
+dias30 =  [4, 6, 9, 11] # Abril, Junio, Septiembre y Noviembre meses con 30 días
+
 
 # R0
 def fecha_es_tupla(p_date: tuple) -> bool:
@@ -11,7 +18,7 @@ def fecha_es_tupla(p_date: tuple) -> bool:
     if((not isinstance(p_date[0], int)) or (not isinstance(p_date[1], int)) or (not isinstance(p_date[2], int))): # Se validan que sean 3 números enteros
         return False
 
-    if(p_date[0] < 0 or p_date[1] < 0 or p_date[2] < 0):
+    if(p_date[0] < 0 or p_date[1] < 0 or p_date[2] < 0): # Validar que el número no sea negativo
         return False
 
     return True
@@ -43,11 +50,11 @@ def fecha_es_valida(p_date: tuple) -> bool:
         return False
 
     ''' Validación de los días de un mes '''
-    if(p_date[1] in [1, 3, 5, 7, 8, 10, 12]): # Enero, Marzo, Mayo, Julio, Agosto, Octubre y Diciembre meses con 31 días
+    if(p_date[1] in dias31): # Si es un mes con 31 días ver que los días no sea mayor a 31
         if(p_date[2] > 31):
             return False
             
-    elif(p_date[1] in [4, 6, 9, 11] and p_date): # Abril, Junio, Septiembre y Noviembre meses con 30 días
+    elif(p_date[1] in dias30): # Si es un mese con 30 días ver que los días no sea mayor a 30
         if(p_date[2] > 30):
             return False
 
@@ -60,39 +67,171 @@ def fecha_es_valida(p_date: tuple) -> bool:
 
     return True
 
+# R3
+def dia_siguiente(p_date: tuple) -> tuple:
+    '''Función que dada una fecha válida determina el día siguiente.'''
+
+    '''Validaciones de la fecha'''
+    if(not fecha_es_tupla(p_date)):
+        raise Exception('La fecha ingresada no es válida: El formato es incorrecto o se tienen número no positivo, no enteros')
+
+    if(not fecha_es_valida(p_date)):
+        raise Exception('La fecha ingresada no es válida: La fecha no es parte del calendario gregoriano')
+
+    p_date_var = (p_date[0], p_date[1], p_date[2] + 1) # Se le agrega uno al día y se valida
+    if(fecha_es_valida(p_date_var)):
+        return p_date_var
+
+    p_date_var = (p_date_var[0], p_date_var[1] + 1, 1) # Si sumandole uno al día es inválido entonces se pasa al siguiente mes
+    if(fecha_es_valida(p_date_var)):
+        return p_date_var
+
+    return (p_date_var[0] + 1, 1, 1) # Si sumandole uno al mes es inválido entonces se pasa al siguiente año
+
+# R4
+def dias_desde_primero_enero(p_date: tuple) -> int:
+    '''Función que determina la cantidad de días transcurridos desde el primero de enero del mismo año'''
+    
+    '''Validaciones de la fecha'''
+    if(not fecha_es_tupla(p_date)):
+        raise Exception('La fecha ingresada no es válida: El formato es incorrecto o se tienen número no positivo, no enteros')
+
+    if(not fecha_es_valida(p_date)):
+        raise Exception('La fecha ingresada no es válida: La fecha no es parte del calendario gregoriano')
+
+    diasTranscurridos = 0
+    hoy = (p_date[0],1,1)
+    while(hoy != p_date):
+        diasTranscurridos += 1
+
+        hoy = dia_siguiente(hoy)
+        
+    return diasTranscurridos
+    
+    
+#R5
+def fecha_hoy() -> tuple:
+    '''Función que retorna la fecha del día de hoy como una tupla de fecha válida'''
+    m_date = date.today()
+    return (m_date.year, m_date.month, m_date.day)
+
+#R6
+def edad_hoy(p_date: tuple) -> tuple:
+    '''Función que determina la edad de una personas en años, meses y dias cumplidos.'''
+    today = fecha_hoy()
+
+    # Encontrar los años cumplidos
+    years = today[0] - p_date[0]
+
+    if(p_date[1] == today[1] and p_date[2] == today[2]):
+        return (years, 0, 0)
+
+    pastDay = True # Encontrar si la persona ya cumplió años en este año
+    if(today[1] < p_date[1]):
+        pastDay = False
+    elif(today[1] == p_date[1]):
+        if(today[2] < p_date[2]):
+            pastDay = False
+
+    months = 0
+    days = 0 
+
+    if(not pastDay):
+        years -= 1
+        months = (12 - p_date[1]) + today[1] # Encontrar los meses cumplidos
+        daysJan = dias_desde_primero_enero(today) # Encontrar los días cumplidos
+        daysDec = 0
+        december = (p_date[0], 12, 31)
+        while(p_date != december):
+            daysDec += 1
+
+            p_date = dia_siguiente(p_date)
+
+        days = daysJan + daysDec
+        
+    else:
+        months = (today[1] - p_date[1]) # Encontrar los meses cumplidos
+        p_date = (today[0], p_date[1], p_date[2]) # Encuentro la cantidad de días cumplidos
+        while(p_date != today):
+            days += 1
+
+            p_date = dia_siguiente(p_date)
+
+    return (years, months, days)
 
 ''' Sección de pruebas para los requerimientos '''
-# R0: fecha_es_tupla
-assert fecha_es_tupla(32) == False
-assert fecha_es_tupla('hola') == False
-assert fecha_es_tupla((-1, -1, -1)) == False
-assert fecha_es_tupla((1, 2, 3, 4)) == False
-assert fecha_es_tupla((1, 2, -3)) == False
-assert fecha_es_tupla((1, 2, 3)) == True
+if __name__ == "__main__":
+    # R0: fecha_es_tupla
+    assert fecha_es_tupla(32) == False
+    assert fecha_es_tupla('hola') == False
+    assert fecha_es_tupla((-1, -1, -1)) == False
+    assert fecha_es_tupla((1, 2, 3, 4)) == False
+    assert fecha_es_tupla((1, 2, -3)) == False
+    assert fecha_es_tupla((1, 2, 3)) == True
 
-# R1: bisiesto
-assert bisiesto(1800) == False
-assert bisiesto(1900) == False
-assert bisiesto(2100) == False
-assert bisiesto(2300) == False
-assert bisiesto(2500) == False
-assert bisiesto(2020) == True
-assert bisiesto(2000) == True
-assert bisiesto(2400) == True
-assert bisiesto(2308) == True
-assert bisiesto(1924) == True
+    # R1: bisiesto
+    assert bisiesto(1800) == False
+    assert bisiesto(1900) == False
+    assert bisiesto(2100) == False
+    assert bisiesto(2300) == False
+    assert bisiesto(2500) == False
+    assert bisiesto(2020) == True
+    assert bisiesto(2000) == True
+    assert bisiesto(2400) == True
+    assert bisiesto(2308) == True
+    assert bisiesto(1924) == True
 
-# R2: fecha_es_valida
-assert fecha_es_valida((1700, 10, 10, 12)) == False
-assert fecha_es_valida((1500, 10, 10)) == False
-assert fecha_es_valida((2500, 2, 29)) == False
-assert fecha_es_valida((2300, 0, 12)) == False
-assert fecha_es_valida((2300, 5, 0)) == False
-assert fecha_es_valida((1900, 14, 12)) == False
-assert fecha_es_valida((1900, 11, 31)) == False
-assert fecha_es_valida((1900, 1, 32)) == False
-assert fecha_es_valida((1924, 2, 29)) == True
-assert fecha_es_valida((2020, 5, 28)) == True
-assert fecha_es_valida((2500, 2, 28)) == True
-assert fecha_es_valida((1998, 12, 13)) == True
-assert fecha_es_valida((2012, 12, 12)) == True
+    # R2: fecha_es_valida
+    assert fecha_es_valida((1700, 10, 10, 12)) == False
+    assert fecha_es_valida((1500, 10, 10)) == False
+    assert fecha_es_valida((2500, 2, 29)) == False
+    assert fecha_es_valida((2300, 0, 12)) == False
+    assert fecha_es_valida((2300, 5, 0)) == False
+    assert fecha_es_valida((1900, 14, 12)) == False
+    assert fecha_es_valida((1900, 11, 31)) == False
+    assert fecha_es_valida((1900, 1, 32)) == False
+    assert fecha_es_valida((1924, 2, 29)) == True
+    assert fecha_es_valida((2020, 5, 28)) == True
+    assert fecha_es_valida((2500, 2, 28)) == True
+    assert fecha_es_valida((1998, 12, 13)) == True
+    assert fecha_es_valida((2012, 12, 12)) == True
+
+    # R3
+    assert dia_siguiente((2012, 12, 31)) == (2013, 1, 1)
+    assert dia_siguiente((1992, 12, 1)) == (1992, 12, 2)
+    assert dia_siguiente((1899, 11, 30)) == (1899, 12, 1)
+    assert dia_siguiente((1792, 12, 31)) == (1793, 1, 1)
+    assert dia_siguiente((1899, 1, 1)) == (1899, 1, 2)
+    assert dia_siguiente((1899, 1, 31)) == (1899, 2, 1)
+    testCase = TestCase()
+    testCase.assertRaises(Exception, dia_siguiente, (1, 1, 1))
+    testCase.assertRaises(Exception, dia_siguiente, (1440, 1, 1))
+    testCase.assertRaises(Exception, dia_siguiente, (0, 0, 0, 0))
+    testCase.assertRaises(Exception, dia_siguiente, 'hola')
+    testCase.assertRaises(Exception, dia_siguiente, (1989, 14, 1))
+    testCase.assertRaises(Exception, dia_siguiente, (1989, 12, 75))
+    
+    # R4
+    assert dias_desde_primero_enero((2020,1,1)) == 0
+    assert dias_desde_primero_enero((2020,1,13)) == 12
+    assert dias_desde_primero_enero((2020,2,13)) == 43
+    assert dias_desde_primero_enero((2019,2,13)) == 43
+    assert dias_desde_primero_enero((2020,3,1)) == 60
+    assert dias_desde_primero_enero((2020,4,1)) == 91
+    assert dias_desde_primero_enero((2020,10,13)) == 286
+    assert dias_desde_primero_enero((2020,11,13)) == 317
+    assert dias_desde_primero_enero((2020,12,25)) == 359
+    assert dias_desde_primero_enero((2019,12,31)) == 364
+    assert dias_desde_primero_enero((2020,12,31)) == 365
+    testCase.assertRaises(Exception, dias_desde_primero_enero, (1400, 12, 75))
+    testCase.assertRaises(Exception, dias_desde_primero_enero, 'hola')
+
+    # R5
+    # Esta prueba requiere de cambiar de cambiar la fecha a mano por lo que se va a comentar para que no afecte al programa
+    # assert fecha_hoy() == (2020, 5, 28)
+
+    # R6
+    assert edad_hoy((1998, 12, 13)) == (21, 5, 166)
+    assert edad_hoy((1998, 9, 30)) == (21, 8, 240)
+    assert edad_hoy((1995, 1, 3)) == (25, 4, 146)
+    assert edad_hoy((1995, 5, 28)) == (25, 0, 0)
